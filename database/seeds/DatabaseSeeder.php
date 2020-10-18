6af55d8588500d2a065c9e53376d35ff11cabfe0
Alex Khayev <alexander.khayev@gmail.com>
2020-10-18 10:59:29 +0400

    add init db, migrate & seed news

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\NewsSeeder\NewsSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(NewsSeeder::class);
+//        $this->call(TestSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/NewsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\NewsSeeder;
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('news')->insert($this->getData());
+    }
+
+    public function getData(): array
+    {
+        $data = [];
+
+        $faker = Factory::create("ru_RU");
+        for ($row = 0; $row < 10; ++$row) {
+            $data[] = [
+                'title' => $faker->sentence(3,6),
+                'message' => $faker->text(1000),
+                'private' => $faker->boolean
+            ];
+        }
+        return $data;
+    }
+}
