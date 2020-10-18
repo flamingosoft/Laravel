3edea5704a366bb2115fe0bf790782fce184b773
Alex Khayev <alexander.khayev@gmail.com>
2020-10-18 09:27:39 +0400

    db settings & dbeaver init

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,10 +1,10 @@
 
-<li class="nav-item"><a class="nav-link {{ request()->routeIs('home')?'active':'' }}" href="{{ route('home') }}">{{ __('Главная') }}</a></li>
-<li class="nav-item"><a class="nav-link {{ request()->routeIs('news')?'active':'' }}" href="{{ route('news.home') }}">{{ __('Новости') }}</a></li>
+<a class="p-2 text-muted {{ request()->routeIs('home')?'active':'' }}" href="{{ route('home') }}">{{ __('Главная') }}</a>
+<a class="p-2 text-muted {{ request()->routeIs('news')?'active':'' }}" href="{{ route('news.home') }}">{{ __('Новости') }}</a>
 
-<li class="nav-item"><a class="nav-link {{ request()->routeIs('admin.home')?'active':'' }}" href="{{ route('admin.home') }}">{{ __('Админка') }}</a></li>
-<li class="nav-item"><a class="nav-link {{ request()->routeIs('admin.addNews')?'active':'' }}" href="{{ route('admin.addNews') }}">{{ __('Добавить новость') }}</a></li>
-<li class="nav-item"><a class="nav-link {{ request()->routeIs('admin.getNews')?'active':'' }}" href="{{ route('admin.getNews') }}">{{ __('Скачать новости') }}</a></li>
+<a class="p-2 text-muted {{ request()->routeIs('admin.home')?'active':'' }}" href="{{ route('admin.home') }}">{{ __('Админка') }}</a>
+<a class="p-2 text-muted {{ request()->routeIs('admin.addNews')?'active':'' }}" href="{{ route('admin.addNews') }}">{{ __('Добавить новость') }}</a>
+<a class="p-2 text-muted {{ request()->routeIs('admin.getNews')?'active':'' }}" href="{{ route('admin.getNews') }}">{{ __('Скачать новости') }}</a>
 
-<li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('Залогиниться') }}</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Зарегаться') }}</a></li>
+<a class="p-2 text-muted" href="{{ route('login') }}">{{ __('Залогиниться') }}</a>
+<a class="p-2 text-muted" href="{{ route('register') }}">{{ __('Зарегаться') }}</a>
