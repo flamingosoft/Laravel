dba31668080b5b8ba69fe648f4153ddbbf255e74
Alex Khayev <alexander.khayev@gmail.com>
2020-11-22 17:52:16 +0400

    Переработана админка

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,9 +5,40 @@
 @endsection
 
 @section('content')
-    <div class="col-md-6 blog-main">
+    <div class="container blog-main">
         <div class="blog-post">
             <h2 class="blog-post-title">{{ __('Админка') }} </h2>
+            <br/>
+
+            <div class="row">
+                <div class="col-sm-6 ">
+                    <h3>Список категорий</h3>
+                    <table class="container col-xs-12">
+                        @forelse($categories as $category)
+                            <tr>
+                                <td class="col-xs-6"><a href="{{ route("news.category.show", $category) }}">{{ $category->title }}</a></td>
+                                <td class="col-xs-3"><a href="{{ route("news.category.edit", $category) }}">Edit</a></td>
+                                <td class="col-xs-3"><a href="{{ \route("news.category.destroy", $category) }}">Delete</a></td>
+                            </tr>
+                        @empty
+                            <p>Нет категорий</p>
+                        @endforelse
+                    </table>
+                </div>
+
+                <div class="col-sm-6 ">
+                    <h3>Список новостей</h3>
+                    <ol>
+                        @forelse($news as $new)
+                            <li>{{ $new->title }}</li>
+                        @empty
+                            <p>Нет новостей</p>
+                        @endforelse
+                    </ol>
+                </div>
+
+            </div>
         </div>
     </div>
+
 @endsection
